83b97904d5fafcd7c4f243a7fa97bd909ef18470
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2024-01-01 19:45:41 +0000

    Add questionsArr to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// questionsArr : store quiz questions
+
+const questionsArr = [
+  {
+    forAsk: '1. Do we only have two ways to declare a variable in JavaScript?',
+    forAnswer: false
+  },
+  {
+    forAsk: '2. Does the operator !== describe that only the value is different?',
+    forAnswer: false
+  },
+  {
+    forAsk: `3. Is the syntax: const obj = ['first', 'second', 'third'] correct?`,
+    forAnswer: true
+  },
+  {
+    forAsk: '4. The if ( ) conditional will only work if it contains the else { } conditional?',
+    forAnswer: false
+  },
+  {
+    forAsk: '5. These are the three data types in JavaScript that cannot contain values: null, typeof and undefined?',
+    forAnswer: false
+  }
+];
